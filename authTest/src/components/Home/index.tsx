d843d3e641d2de1c3f120d7cba666231a596c807
Robin Webber <robinewebber@gmail.com>
2019-12-02 10:31:59 -0500

    Home properly typed

--- authTest/src/components/Home/index.tsx
+++ authTest/src/components/Home/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { NextPage, NextPageContext } from "next";
 
-const Home = () => {
-  return <h1>Hello World</h1>;
+type HomeProps = ComponentProps & NextProps;
+
+type ComponentProps = {
+  userAgent: string;
+};
+
+type NextProps = {};
+
+const Home: NextPage<HomeProps, NextProps> = ({ userAgent }) => {
+  return <h1>Hello World - user agent: {userAgent}</h1>;
+};
+
+Home.getInitialProps = async ({ req }: NextPageContext) => {
+  const userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
+  return { userAgent };
 };
 
 export default Home;
